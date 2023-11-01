87f2cee314461dd1934265b911a74af32014d3d5
myeongki <myeongki114@naver.com>
2023-11-01 20:31:43 +0900

    intro

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { React } from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "./app/Header";
 import Index from "./app/Index";
+import Intro from "./app/Intro";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Header />}>
         <Route path="/" element={<Index />} />
+        <Route path="/intro" element={<Intro />} />
       </Route>
     </Routes>
   );
