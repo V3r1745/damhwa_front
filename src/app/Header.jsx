b21f6c82d6bc7a4e4f9b9ea6d92ad57406733c83
Seo Dong min <gundam3331@knou.ac.kr>
2023-11-01 16:12:53 +0900

    index html style css

--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 class Header extends React.Component {
   render() {
     return (
       <>
+        <header>
+          <Link to="/소개">소개</Link>
+          <Link to="/상점">상점</Link>
+          <Link to="/">
+            <img src="" alt="" />
+          </Link>
+          <Link to="/행사">행사</Link>
+          <Link to="/FAQ">FAQ</Link>
+        </header>
         <Outlet />
       </>
     );
