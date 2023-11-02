77666436344c96c4693ee826c049ef11f113224a
chan <baekgyeon@naver.com>
2023-11-02 17:32:39 +0900

    main page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./app/Header";
 import Intro from "./app/Intro";
 import Pro from "./app/Pro";
+import Main from "./app/Main";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Header />}>
-        <Route path="/" element={<Intro />} />
+        <Route path="/" element={<Main />} />
         <Route path="/intro" element={<Intro />} />
         <Route path="/pro" element={<Pro />} />
       </Route>
---
+++ src/app/Main.jsx
@@ -0,0 +1,11 @@
+import Example from "./MainElement";
+
+const MainPage = () => {
+  return <>
+    <main className="main-main">
+      <Example />
+    </main>
+  </>
+}
+
+export default MainPage;
