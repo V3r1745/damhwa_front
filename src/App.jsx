f95cae9d845eb204213f1a25e621e7766018bfda
Seo Dong min <gundam3331@knou.ac.kr>
2023-11-03 19:12:50 +0900

    chat bot update

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,21 @@ import Bot from "./app/bot/Bot";
 import MainPage from "./app/mainPage/Main";
 import EventPage from "./app/eventPage/Event";
 import EventAllPage from "./app/eventPage/EventAll";
-import ProKitchen from "./app/proPage/proCategory/Kitchen"
-import ProLiving from "./app/proPage/proCategory/Living"
-import ProBathroom from "./app/proPage/proCategory/Bathroom"
-import ProStorage from "./app/proPage/proCategory/Storage"
-import ProClean from "./app/proPage/proCategory/Clean"
-import ProWashing from "./app/proPage/proCategory/Washing"
+import ProKitchen from "./app/proPage/proCategory/Kitchen";
+import ProLiving from "./app/proPage/proCategory/Living";
+import ProBathroom from "./app/proPage/proCategory/Bathroom";
+import ProStorage from "./app/proPage/proCategory/Storage";
+import ProClean from "./app/proPage/proCategory/Clean";
+import ProWashing from "./app/proPage/proCategory/Washing";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Header />}>
         <Route path="/" element={<MainPage />} />
-        <Route path="/intro" element={<Intro />} />
+        <Route path="/intro" element={<Intro />}>
+          {/* <Route path="/intro/bot" element={<Bot />} /> */}
+        </Route>
         <Route path="/pro" element={<Pro />}>
           <Route path="/pro/kitchen" element={<ProKitchen />} />
           <Route path="/pro/living" element={<ProLiving />} />
@@ -29,10 +31,11 @@ const App = () => {
           <Route path="/pro/clean" element={<ProClean />} />
           <Route path="/pro/washing" element={<ProWashing />} />
         </Route>
+        {/* <Route path="/pro/bot" element={<Pro><Bot /></Pro>} /> */}
         <Route path="/proDetail" element={<ProDetail />} />
         <Route path="/event" element={<EventPage />} />
         <Route path="/eventAll" element={<EventAllPage />} />
-        <Route path="/bot" element={<Bot />} />
+        {/* <Route path="/bot" element={<Bot />} /> */}
       </Route>
     </Routes>
   );
