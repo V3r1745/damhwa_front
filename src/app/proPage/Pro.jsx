18f2eeef023b09eb3d02cb27b320bee19854dca8
chan <baekgyeon@naver.com>
2023-11-03 18:13:39 +0900

    ang

--- src/app/proPage/Pro.jsx
+++ src/app/proPage/Pro.jsx
@@ -6,12 +6,12 @@ const Pro = () => {
     <main className="pro-main">
       <aside>
         <p>카테고리</p>
-        <Link to="/pro/kitchen">주방용품</Link>
-        <Link to="/pro/living">생활용품</Link>
-        <Link to="/pro/bathroom">욕실용품</Link>
-        <Link to="/pro/storage">수납/정리용품</Link>
-        <Link to="/pro/clean">청소용품</Link>
-        <Link to="/pro/washing">세탁용품</Link>
+        <Link to="/pro/kitchen/1">주방용품</Link>
+        <Link to="/pro/living/1">생활용품</Link>
+        <Link to="/pro/bathroom/1">욕실용품</Link>
+        <Link to="/pro/storage/1">수납/정리용품</Link>
+        <Link to="/pro/clean/1">청소용품</Link>
+        <Link to="/pro/washing/1">세탁용품</Link>
       </aside>
       <article>
         <section>
