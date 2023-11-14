2fbfa0c5cbf9407d00908f09fa7271b22e4badee
Seo Dong min <gundam3331@knou.ac.kr>
2023-11-14 15:15:24 +0900

    change main

--- src/app/mainPage/mainPageM/MainEventM.jsx
+++ src/app/mainPage/mainPageM/MainEventM.jsx
@@ -1,19 +1,32 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const MainEventElement = () => {
-  return <>
-    <p className="today-event-suggestion">최근 행사 추천</p>
-    <div className="today-event-wrap">
-      <div className="each-event-box">
-        <img src="./src/img/main/1page_img1.jpg" alt="" />
-        <p className="each-event-title">2023 환경창업대전</p>
-        <p className="each-event-date">행사기간 : 2024.06 </p>
+  const [element, setElement] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const result = await axios.get("/get_festa");
+      setElement(result.data);
+      console.log(result.data);
+    })();
+  }, []);
+  return (
+    <>
+      <p className="today-event-suggestion">최근 행사 추천</p>
+      <div className="today-event-wrap">
+        <div className="each-event-box">
+          <img src={`/src/img/festa/${element.at(0)?.fields.festival_img.at(0)}`} alt="" />
+          <p className="each-event-title">{element.at(0)?.fields.festival_title}</p>
+          <p className="each-event-date">행사기간 : {element.at(0)?.fields.festival_date}</p>
+        </div>
+        <div className="each-event-box">
+          <img src={`/src/img/festa/${element.at(1)?.fields.festival_img.at(0)}`} alt="" />
+          <p className="each-event-title">{element.at(1)?.fields.festival_title}</p>
+          <p className="each-event-date">행사기간 : {element.at(1)?.fields.festival_date}</p>
+        </div>
       </div>
-      <div className="each-event-box">
-        <img src="./src/img/main/1page_img1.jpg" alt="" />
-        <p className="each-event-title">2023 환경창업대전</p>
-        <p className="each-event-date">행사기간 : 2024.06</p>
-      </div>
-    </div>
-  </>;
+    </>
+  );
 };
 
 export default MainEventElement;
